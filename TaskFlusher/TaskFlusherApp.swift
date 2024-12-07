9e4605d9547bfe0789ab26150cafe4f04393b42e
Kanta Oikawa <k.oikawa@kantacky.com>
2024-12-07 21:26:28 +0900

    Add Xcode project

---
+++ TaskFlusher/TaskFlusherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlusherApp.swift
+//  TaskFlusher
+//
+//  Created by Kanta Oikawa on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlusherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskFlusherTests/TaskFlusherTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlusherTests.swift
+//  TaskFlusherTests
+//
+//  Created by Kanta Oikawa on 12/7/24.
+//
+
+import Testing
+@testable import TaskFlusher
+
+struct TaskFlusherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
